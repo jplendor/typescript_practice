f800b24693573c731a408c07ebb72f5136330194
jplendor <jplendor@gmail.com>
2022-04-13 23:48:54 +0900

    타입스크립트를 쓰는 이유

---
+++ test.js
@@ -0,0 +1,9 @@
+let dynamic_type_var = 'JS'
+
+console.log(typeof dynamic_type_var)
+console.log(dynamic_type_var.charAt(0))
+
+dynamic_type_var = 123
+console.log(typeof dynamic_type_var)
+// console.log(dynamic_type_var.charAt(0)) 
+// 런타임 단계에서 TypeError 발생
---
+++ test.ts
@@ -0,0 +1,4 @@
+let static_type_var : String = 'typescript'
+
+// static_type_var = 123
+// 컴파일 단계에서 미리 에러 확인 가능
